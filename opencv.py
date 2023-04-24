0df705755aa50e220cb74489dec00aced61e5e65
smitdumore <smit.dumore17@vit.edu>
2023-04-24 17:08:52 -0400

    added ellipse

--- opencv.py
+++ opencv.py
@@ -13,8 +13,8 @@ tab_width = 600
 tab_height = 200
 
 def get_inquality_obstacles(x, y, clearance):
-    if (((x-400)**2 + (y-110)**2) < (60 + clearance)**2) or\
-        ((x-200)**2 + (y-110)**2) < (60 + clearance)**2 or\
+    if (((x-400)**2 + (y-110)**2) < (10 + clearance)**2) or\
+        ((x-200)**2 + (y-110)**2) < (10 + clearance)**2 or\
         (x >= (tab_width - clearance)) or (y >= (tab_height - clearance)) or (x <= clearance) or (y <= clearance):
             return True
     
@@ -29,7 +29,7 @@ class Node:
 
 class RRT:
     def __init__(self, start, goal, clearance, expand_dis= 30,
-                 goal_sample_rate=10, max_iter=6000):
+                 goal_sample_rate=10, max_iter=2000):
 
         self.start_node = start
         self.goal_node = goal
@@ -43,7 +43,7 @@ class RRT:
         self.node_list = None
         self.path = []
         self.flag = False
-        self.colo = 0
+        self.colo = 100
         
 
     def distance(self, node1, node2):
@@ -136,7 +136,7 @@ class RRT:
                 near_nodes.append(n)
         return near_nodes
 
-    def rewire(self, node):
+    def rewire(self, node, img):
         
         # see if new node can be a new parent to already exsisting node
 
@@ -154,6 +154,13 @@ class RRT:
             if new_cost < near_node.cost:
                 near_node.parent = node
                 near_node.cost = new_cost
+                ## erase
+                cv2.line(img, (node.x, node.y),(node.parent.x, node.parent.y),(0, 0, 0), 1)
+
+                ## draw new
+                cv2.line(img, (near_node.x, near_node.y),(node.parent.x, node.parent.y),(255, 0, 0), 1)
+
+
 
     def search(self):
 
@@ -164,7 +171,10 @@ class RRT:
         
         self.node_list.append(start_node)
 
+        ### CANVAS ###
         img = np.zeros((200, 600, 3), dtype=np.uint8)
+        ### CANVAS ###
+
         cv2.circle(img, (start_node.x, start_node.y), 5, (255, 0, 0), -1)
         cv2.circle(img, (goal_node.x, goal_node.y), 5, (0, 255, 0), -1)  
 
@@ -183,6 +193,7 @@ class RRT:
         a1 = np.array([[(self.goal_node.x - self.start_node.x) / c_min],
                        [(self.goal_node.y - self.start_node.y) / c_min], [0]])
 
+
         e_theta = math.atan2(a1[1], a1[0])
         # first column of identity matrix transposed
         id1_t = np.array([1.0, 0.0, 0.0]).reshape(1, 3)
@@ -192,8 +203,9 @@ class RRT:
             [1.0, 1.0,
              np.linalg.det(u) * np.linalg.det(np.transpose(vh))]) @ vh
 
-
-
+        ##################
+        #### RRT LOOP ####
+        ##################
         for i in range(self.max_iter):
             
             rand_node = self.informed_sample(c_best, c_min, x_center, c)
@@ -212,7 +224,8 @@ class RRT:
                 #print("showing rand node")
 
                 new_node.parent = nearest_node
-
+                
+                #cv2.line(img, (nearest_node.x, nearest_node.y),(new_node.x, new_node.y),(255, 0, 0), 1)
                 cv2.circle(img, (new_node.x, new_node.y), 1, (0, 0, 255), -1)
                 #time.sleep(2)
                 cv2.imshow("RRT Tree", img)
@@ -238,7 +251,7 @@ class RRT:
 
                 self.node_list.append(new_node)
                 
-                self.rewire(new_node)
+                self.rewire(new_node, img)
             
             else:
 
@@ -256,6 +269,10 @@ class RRT:
 
                 temp_path = self.extract_path(goal_node)
                 temp_path_len = self.get_path_len(temp_path)
+
+                if c_best != float('inf'):
+                    print("plotting ellipse...................")
+                    self.plot_ellipse(img, x_center, c_best, c_min, e_theta)
 
                 if temp_path_len < c_best:
                     self.path = temp_path
@@ -280,19 +297,34 @@ class RRT:
 
         if(self.flag):
             cv2.imshow("RRT Tree", img)
-            cv2.waitKey(100000)
-            # print("path visualising")
-            # for i in range(self.path.shape[0] - 1):
-            #         cv2.line(img, (int(self.path[i, 0]), int(self.path[i, 1])),
-            #          (int(self.path[i + 1, 0]), int(self.path[i + 1, 1])),
-            #          (0, 255, 0), 2)
-                    
-            # cv2.imshow("RRT Tree", img)
-            # cv2.waitKey(100000)
+            cv2.waitKey(100)
 
-        #else:
-        #    print("iter finished")
-        return None
+        return self.path
+    
+    def rot_mat_2d(self, theta):
+        c, s = np.cos(theta), np.sin(theta)
+        return np.array([[c, -s], [s, c]])
+    
+    def plot_ellipse(self,canvas, x_center, c_best, c_min, e_theta):
+
+        a = math.sqrt(c_best ** 2 - c_min ** 2) / 2.0
+        b = c_best / 2.0
+        angle = math.pi / 2.0 - e_theta
+        cx = int(x_center[0])
+        cy = int(x_center[1])
+        t = np.arange(0, 2 * math.pi + 0.1, 0.1)
+        x = [a * math.cos(it) for it in t]
+        y = [b * math.sin(it) for it in t]
+        fx = self.rot_mat_2d(-angle) @ np.array([x, y])
+        px = np.array(fx[0, :] + cx).flatten()
+        py = np.array(fx[1, :] + cy).flatten()
+        # assuming 'canvas' is your OpenCV image, use cv2.polylines to draw the ellipse
+        pts = np.array(list(zip(px, py)), np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(canvas,[pts],True,(0,255,255))
+
+
+
         
     def check_collision(self, nearest_node, new_node):
         """
@@ -302,8 +334,11 @@ class RRT:
         x2, y2 = new_node.x, new_node.y
 
         # Discretize the line segment into points
-        dx = abs(x2 - x1)
-        dy = abs(y2 - y1)
+        #dx = abs(x2 - x1)
+        #dy = abs(y2 - y1)
+
+        dx = math.ceil(x2 - x1)
+        dy = math.ceil(y2 - y1)
         
         steps = 10
         
@@ -312,8 +347,11 @@ class RRT:
 
         # Check for collision with each point on the line segment
         for i in range(int(steps) + 1):
-            x = int(x1 + i * x_step)
-            y = int(y1 + i * y_step)
+            #x = int(x1 + i * x_step)
+            #y = int(y1 + i * y_step)
+
+            x = math.ceil(x1 + i * x_step)
+            y = math.ceil(y1 + i * y_step)
 
             # Check if the point collides with any obstacles
             if get_inquality_obstacles(x, y, self.clearance):
@@ -340,7 +378,7 @@ class RRT:
 
 
 
-def main():
+def IRRT_main():
 
     start_x = int(50)
     start_y = int(100)
@@ -367,6 +405,8 @@ def main():
 
     path = obj.search()
 
+    return path
+
     
 
-main()
+print(IRRT_main())
